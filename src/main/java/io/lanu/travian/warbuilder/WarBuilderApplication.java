0cce2430aadc0ff7d9d8b70f1875eedb110522f7
mrlanu <mrlanu@gmail.com>
2019-12-17 15:32:27 -0600

    init

---
+++ src/main/java/io/lanu/travian/warbuilder/WarBuilderApplication.java
@@ -0,0 +1,13 @@
+package io.lanu.travian.warbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/lanu/travian/warbuilder/WarBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lanu.travian.warbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
