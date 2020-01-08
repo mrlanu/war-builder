0c1949b0e39b4702fd608ba5c772af5f8aa4c65d
mrlanu <mrlanu@gmail.com>
2020-01-07 21:50:28 -0600

    just test

--- src/main/java/io/lanu/travian/warbuilder/services/AttacksServiceImpl.java
+++ src/main/java/io/lanu/travian/warbuilder/services/AttacksServiceImpl.java
@@ -27,7 +27,6 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
